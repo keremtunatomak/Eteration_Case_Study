70749b465357e9129edeb914fa0e1ae24e750ff6
Kerem Tuna Tomak <keremtuna@vizitel.com.tr>
2025-07-06 17:08:10 +0300

    Initial Commit

---
+++ Eteration_Case_Study/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Eteration_Case_Study
+//
+//  Created by Kerem Tuna Tomak on 6.07.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
